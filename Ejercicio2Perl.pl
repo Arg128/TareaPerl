4242366143bfc6a834b813eaf34a2d1be8cf7609
Angel Julio Retamozo Calatayud <aretamozoc@unsa.edu.pe>
2024-10-16 23:40:44 -0500

    Update Ejercicio2Perl.pl 1.5 final

--- Ejercicio2Perl.pl
+++ Ejercicio2Perl.pl
@@ -1,41 +1,40 @@
 #!/usr/bin/perl -w
+use strict;
+use warnings;
 use List::Util qw( min max );
 
 sub llenarNotas {
     my $var = $_[0];
-    @nota;
-    for ($indice = 0 ; $indice < $var*2 - 1; $indice++) {
-        print "Ingrese la nota N\°".$indice." :";
-        ## $nota[$indice] = <STDIN>;
-        $entrada = <STDIN>;
+    my @nota;
+    for (my $indice = 0; $indice < $var * 2 - 1; $indice += 2) {
+        print "Ingrese la nota N\°".($indice/2 + 1).": ";
+        my $entrada = <STDIN>;
+        chomp($entrada);
         $nota[$indice] = $entrada;
-        $nota[$indice + 1] = $indice;
-        $indice = $indice + 1;
+        $nota[$indice + 1] = $indice / 2 + 1;
     }
     return @nota;
 }
-@notas = llenarNotas(4);
-%hash = @notas;
-## %hash{$entrada} =  $indice;
-$mayor_1 = max(@notas);
-$menor_1 = min(@notas);
 
-$notas[%hash{$menor_1}] = $mayor_1;
+my @notas = llenarNotas(4);
+my %hash = @notas;
+
+my $mayor_1 = max(values %hash);
+my $menor_1 = min(values %hash);
+$hash{$menor_1} = $mayor_1;
 
 sub promedio {
-    $suma = 0;
-    $intit = 0
-    my (@array) = $_[0];
-    for ($i = 1 ; $i < scalar(@array) ; $i++) {
-        $suma = $suma + $array[$i];
-        $i = $i + 1;
-        $init = $init + 1;
-        print $init;
+    my @array = @_;
+    my $suma = 0;
+    my $count = 0;
+    for (my $i = 0; $i < scalar(@array); $i += 2) {
+        $suma += $array[$i];
+        $count++;
     }
-    return $suma/($init);
+    return $suma / $count;
 }
 
-print promedio(@notas)
+print promedio(values %hash);
 
 
 
