646a2a748a87921b9c1fd8447bafc9fcc8fad309
Angel Julio Retamozo Calatayud <aretamozoc@unsa.edu.pe>
2024-10-16 22:49:29 -0500

    Update Ejercicio2Perl.pl 1.3

--- Ejercicio2Perl.pl
+++ Ejercicio2Perl.pl
@@ -2,15 +2,13 @@
 use List::Util qw( min max );
 
 @nota;
-%hash;
 for ($indice = 0 ; $indice < 4 ; $indice++) {
     print "Ingrese la nota N\°".$indice." :";
     ## $nota[$indice] = <STDIN>;
     $entrada = <STDIN>;
     $nota[$indice] = $entrada;
-    unshift(@nota, $entrada);
-    unshift(@nota, $indice);
-    
+    $nota[$indice + 1] = $indice;
+    $indice = $indice + 1;
 }
 
 %hash = @nota;
@@ -20,18 +18,14 @@ $menor_1 = min(@nota);
 
 $nota[%hash{$menor_1}] = $mayor_1;
 
-for ($indice = 1 ; $indice < scalar(@nota) ; $indice++) {
-    @nota[$indice] = undef;
-    $indice = $indice + 1;
-}
-
 print @nota;
 
 sub promedio {
     $suma = 0;
     my (@array) = $_[0];
-    for ($i = 0 ; $i < scalar(@array) ; $i++) {
+    for ($i = 1 ; $i < scalar(@array) ; $i++) {
         $suma = $suma + $array[$i];
+        $i = $i + 1;
     }
     return $suma/($i);
 }
